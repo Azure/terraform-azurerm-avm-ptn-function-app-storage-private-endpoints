a629c0375d53cd6c813b0bba6a8f0104711b287b
Donovan McCoy <mccoy.donov@gmail.com>
2026-02-04 11:53:57 -0500

    changes with updates to default testing

--- variables.storage_account.tf
+++ variables.storage_account.tf
@@ -356,7 +356,7 @@ variable "storage_account" {
     })), {})
     role_assignments = optional(map(object({
       role_definition_id_or_name             = string
-      principal_id                           = optional(string)
+      principal_id                           = string
       description                            = optional(string, null)
       skip_service_principal_aad_check       = optional(bool, false)
       condition                              = optional(string, null)
